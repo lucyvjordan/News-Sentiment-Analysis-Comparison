e0921d1246bb32a98174a9687ab08a85c948cbb0
Lucy <86916789+lucyvjordan@users.noreply.github.com>
2023-08-26 17:31:17 +0100

    Headlines scrape of The Guardian

--- webscrape.py
+++ webscrape.py
@@ -15,3 +15,22 @@ for title in container.select('h3[class*="promo-heading__title"]'):
     if title.get_text() not in bbctitles:
         bbctitles.append(title.get_text()) 
 print(bbctitles)
+
+
+# The Guardian
+
+newswebsite = "https://www.theguardian.com/uk"
+
+website = requests.get(newswebsite)
+
+soup = BeautifulSoup(website.content, "html.parser")
+
+container = soup.find("div", id="container-headlines")
+
+guardiantitles = []
+for title in container.find_all('a'):
+    try: 
+        guardiantitles.append(title['aria-label']) # check this is working with new headlines tomorrow
+    except:
+        pass # because not a headline text (all headlines texts have aria-labels)
+print(guardiantitles)
