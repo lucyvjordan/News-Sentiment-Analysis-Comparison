6827e10b91a9dc8960ed34a6e3598c05dc78670b
Lucy <86916789+lucyvjordan@users.noreply.github.com>
2023-08-27 21:10:32 +0100

    Headlines scrape of Sky News + code rearrange

--- webscrape.py
+++ webscrape.py
@@ -1,36 +1,73 @@
 import requests
 from bs4 import BeautifulSoup
 
-# BBC
+import analysis
 
-newswebsite = "https://www.bbc.co.uk/news"
-website = requests.get(newswebsite)
-soup = BeautifulSoup(website.content, "html.parser")
-
-container = soup.find("div", id="latest-stories-tab-container")
-# id = "news-top-stories-container" -- for only top stories
-
-bbctitles = []
-for title in container.select('h3[class*="promo-heading__title"]'):
-    if title.get_text() not in bbctitles:
-        bbctitles.append(title.get_text()) 
-print(bbctitles)
+websites = {"BBC": "www.bbc.co.uk/news", 
+            "The Guardian": "www.theguardian.com/uk",
+            "Sky News": "news.sky.com"}
 
 
-# The Guardian
+def scrape_headlines(websites):
 
-newswebsite = "https://www.theguardian.com/uk"
+    allScores = []
+    aggregateScores = []
 
-website = requests.get(newswebsite)
+    for x in websites:
 
-soup = BeautifulSoup(website.content, "html.parser")
+        websiteURL = "https://" + websites[x]
+        # creates URL for web request using values in dictionary
 
-container = soup.find("div", id="container-headlines")
+        website = requests.get(websiteURL)
+        # connects to website
 
-guardiantitles = []
-for title in container.find_all('a'):
-    try: 
-        guardiantitles.append(title['aria-label']) # check this is working with new headlines tomorrow
-    except:
-        pass # because not a headline text (all headlines texts have aria-labels)
-print(guardiantitles)
+        soup = BeautifulSoup(website.content, "html.parser")
+        # BeautifulSoup module makes extracting data from website easier
+
+        # news websites are separated because the way to direct the soup to the headlines differs between websites
+        # BBC
+        if x == "BBC":
+            
+            container = soup.find("div", id="news-top-stories-container")
+            # identifies div where main stories are
+
+            headlines = []
+            for headline in container.select('h3[class*="promo-heading__title"]'):
+                if headline.get_text() not in headlines:
+                    # in case headlines are repeated
+                    headlines.append(headline.get_text()) 
+
+
+        # THE GUARDIAN
+        elif x == "The Guardian":
+            container = soup.find("div", id="container-headlines")
+
+            headlines = []
+            for headline in container.find_all('a'):
+                try: 
+                    headlines.append(headline['aria-label'])
+                except:
+                    pass # because not a headline text (all headlines texts have aria-labels)
+
+
+        # SKY NEWS
+        elif x == "Sky News":
+            container = soup.find("div", class_ = "sdc-site-tiles__group")
+
+            headlines = []
+            for headline in container.select('span[class="sdc-site-tile__headline-text"]'):
+                headlines.append(headline.get_text())
+
+
+        headlineAnalysis = analysis.Analyse(headlines)
+        # uses Analyse() function from analysis.py to analyse all of the headlines
+
+        allScores.append(headlineAnalysis[0])
+        # contains positive, neutral, negative and compound
+        aggregateScores.append(headlineAnalysis[1])
+        # is the average of all the compound scores of the headlines
+
+    print(allScores, aggregateScores)
+      
+scrape_headlines(websites)
+
