527b03e660446cfc6333266e6eeb9ae17c3857c7
Lucy <86916789+lucyvjordan@users.noreply.github.com>
2023-08-27 21:09:58 +0100

    Returns average score from all headlines

--- analysis.py
+++ analysis.py
@@ -3,12 +3,18 @@ nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 analyzer = SentimentIntensityAnalyzer()
 
-def Analyze(headlines):
+
+def Analyse(headlines):
 
     sentimentScores = []
-    
+    totalScore = 0
+
     for line in headlines:
         scores = analyzer.polarity_scores(line)
         sentimentScores.append(scores)
+        totalScore += scores['compound']
     
-    return sentimentScores
+    averageScore = round(totalScore / len(headlines), 3)
+
+    return sentimentScores, averageScore
+
