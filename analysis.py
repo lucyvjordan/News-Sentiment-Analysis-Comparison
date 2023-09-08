306ae8691322e4cb900e34699a8dd7730506ff00
Lucy <86916789+lucyvjordan@users.noreply.github.com>
2023-09-08 15:38:04 +0100

    Return data as dictionary

--- analysis.py
+++ analysis.py
@@ -6,15 +6,21 @@ analyzer = SentimentIntensityAnalyzer()
 
 def Analyse(headlines):
 
-    sentimentScores = []
-    totalScore = 0
+    sentimentScores = {'neg': 0, 'neu': 0, 'pos': 0, 'compound': 0}
 
     for line in headlines:
         scores = analyzer.polarity_scores(line)
-        sentimentScores.append(scores)
-        totalScore += scores['compound']
+        # analyse the headline using vader
+
+        sentimentScores['neg'] += scores['neg']
+        sentimentScores['neu'] += scores['neu']
+        sentimentScores['pos'] += scores['pos']
+        sentimentScores['compound'] += scores['compound']
+        # increase total negative, neutral, positive and compound (average) scores
     
-    averageScore = round(totalScore / len(headlines), 3)
+    for key in sentimentScores.keys():
+        # for each category (negative/neutral/positive/compound)
+        sentimentScores[key] = round(sentimentScores[key] / len(headlines), 3)
+        # find the average by dividing by number of headlines
 
-    return sentimentScores, averageScore
-
+    return sentimentScores
